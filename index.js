c35f637fe4de0515de97eef502b8d3611afa4558
Anishkulkarni026 <131292802+Anishkulkarni026@users.noreply.github.com>
2025-06-06 23:12:20 +0530

    index.js

--- index.js
+++ index.js
@@ -180,5 +180,6 @@ app.get('/weather', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(\`Server running on port \${PORT}\`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
+
 
